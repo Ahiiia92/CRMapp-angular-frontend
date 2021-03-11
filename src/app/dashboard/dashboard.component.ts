c6549007125612e36e57b0a3e54ce9a93dc94781
Maroussia Arnault <maroussia.arnault@gmail.com>
2021-03-11 15:22:32 +0100

    contact dynamically added to Dashboard + navigation for contacts done

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+import {Contact} from "../shared/contact.model";
+import {ContactService} from "../shared/services/contact.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.sass']
 })
 export class DashboardComponent implements OnInit {
+  contacts: Observable<Contact[]>
 
-  constructor() { }
+  constructor(private contactService: ContactService,
+              private reouter: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.reloadData();
   }
 
+  reloadData() {
+    this.contacts = this.contactService.getAll();
+  }
+
+  onNewContact() {
+    this.reouter.navigate(['new'], { relativeTo: this.route });
+  }
 }
