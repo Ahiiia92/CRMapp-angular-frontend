559cf9d5dde06f055e945db3391fe3035f3e62cd
Maroussia Arnault <maroussia.arnault@gmail.com>
2021-03-18 16:24:44 +0100

    setup for production done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://crm-real-estate.herokuapp.com/',
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoibWFyb3Vzc2lhIiwiYSI6ImNramlyYzk1bzFjcTAycW83eGd2aWVzcTIifQ.vCTJlBxYKvlYdO4Zvx5CoQ'
+  }
 };
