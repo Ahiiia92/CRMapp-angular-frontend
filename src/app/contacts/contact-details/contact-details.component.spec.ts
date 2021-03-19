9fe7e8a5a043e391a149dc13f9978549faad1100
Maroussia Arnault <maroussia.arnault@gmail.com>
2021-03-19 13:58:37 +0100

    edited test contact details

--- src/app/contacts/contact-details/contact-details.component.spec.ts
+++ src/app/contacts/contact-details/contact-details.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactDetailsComponent } from './contact-details.component';
+import {ContactService} from "../../shared/services/contact.service";
 
 describe('ContactDetailsComponent', () => {
   let component: ContactDetailsComponent;
   let fixture: ComponentFixture<ContactDetailsComponent>;
+  let contactServ: ContactService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,6 +18,7 @@ describe('ContactDetailsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ContactDetailsComponent);
     component = fixture.componentInstance;
+    contactServ = fixture.debugElement.injector.get(contactServ);
     fixture.detectChanges();
   });
 
