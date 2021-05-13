073a0f8025ff9e03cd706b0baf692ccc8339290a
Maroussia Arnault <maroussia.arnault@gmail.com>
2021-05-13 19:41:43 +0200

    added viewing components to the project

---
+++ src/app/viewings/viewing-details/viewing-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewing-details',
+  templateUrl: './viewing-details.component.html',
+  styleUrls: ['./viewing-details.component.sass']
+})
+export class ViewingDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/viewings/viewings.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ViewingsComponent } from './viewings.component';
+
+describe('ViewingsComponent', () => {
+  let component: ViewingsComponent;
+  let fixture: ComponentFixture<ViewingsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ViewingsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ViewingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
