09be7b3c0763adf8e1326b030e5be6f5b23aaa1c
Maroussia Arnault <maroussia.arnault@gmail.com>
2021-05-25 22:18:21 +0200

    added viewing services, models, creating viewing edit component

---
+++ src/app/viewings/viewing-edit/viewing-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewing-edit',
+  templateUrl: './viewing-edit.component.html',
+  styleUrls: ['./viewing-edit.component.sass']
+})
+export class ViewingEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/viewings/viewings.component.ts
+++ src/app/viewings/viewings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-viewings',
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./viewings.component.sass']
 })
 export class ViewingsComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
