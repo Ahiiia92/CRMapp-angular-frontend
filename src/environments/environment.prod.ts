2473c80ddb18b6c1042241cf80a8f324accd43e4
Maroussia Arnault <maroussia.arnault@gmail.com>
2022-02-08 12:56:11 +0100

    authentication feature

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://crm-real-estate.herokuapp.com/api/v1',
-  mapbox: {
-    accessToken: 'pk.eyJ1IjoibWFyb3Vzc2lhIiwiYSI6ImNramlyYzk1bzFjcTAycW83eGd2aWVzcTIifQ.vCTJlBxYKvlYdO4Zvx5CoQ'
-  }
+  apiUrl: 'https://crm-real-estate.herokuapp.com/api/v1'
 };
