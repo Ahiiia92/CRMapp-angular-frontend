94d4072355c886034f289cff155620bac5cc7b14
Maroussia Arnault <maroussia.arnault@gmail.com>
2022-02-10 20:22:29 +0100

    updated the login and app module

---
+++ src/app/auth/api.interceptor.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor, HttpErrorResponse
+} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
+import {catchError, retry} from "rxjs/operators";
+
+@Injectable()
+export class ApiInterceptor implements HttpInterceptor {
+
+  constructor(private cookieService: CookieService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    request = request.clone({
+      setHeaders: {
+        'x-auth': this.cookieService.get('currentUser'),
+        'Cache-Control': 'no-cache',
+        'Pragma': 'no-cache',
+        'Expires': 'Sat, 01 Jan 2000 00:00:00 GMT'
+      }
+    });
+    return next.handle(request)
+      .pipe(
+        retry(1),
+        catchError((error: HttpErrorResponse) => {
+          let errorMessage = '';
+          if (error.error instanceof ErrorEvent) {
+            // client-side error
+            errorMessage = `Error: ${error.error.message}`;
+          } else {
+            // server-side error
+            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+          }
+          window.alert(errorMessage);
+          return throwError(errorMessage);
+        })
+      );
+  }
+}
